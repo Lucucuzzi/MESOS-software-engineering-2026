5d54d5c1498e969e152e4fd3e45ec72154b07004
RiccardoBresciani <riccardo1.bresciani@mail.polimi.it>
2026-03-16 19:12:14 +0100

    aggiunta metodi board e metodi deck (vuoti ihihihih)

--- src/main/java/it/polimi/ingsw/am46/model/Board.java
+++ src/main/java/it/polimi/ingsw/am46/model/Board.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.am46.model.cards.TribeCard;
 import it.polimi.ingsw.am46.model.cards.buildingCards.BuildingCard;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Board {
     private ArrayList<Card> topRow;
@@ -15,4 +16,76 @@ public class Board {
     private Deck<BuildingCard> buildingsEra3;
     private ArrayList<Card> discardCards;
     private ArrayList<Player> totemOrder;
+
+    public Board(){
+        //costruttore valutiamo come usarlo
+    }
+
+    public void discardUnder() {
+        // implementare
+    }
+
+
+    public LinkedList<Color> getTotemOnTrack() {
+        // implement
+        return null;
+    }
+
+    public void setupOfferTile(int numPlayers) {
+        // implement
+    }
+
+    public void setupOrderTile(int numPlayers) {
+        // implement
+    }
+
+    public void setupTribeDeck(int numPlayers) {
+        // implement
+    }
+
+    public void setupUpperRow(int numPlayers) {
+        // implement
+    }
+
+    public void restoreUpperRow(int numPlayers) {
+        // implement
+    }
+
+    public void setupBottomRow(int numPlayers) {
+        // implement
+    }
+
+    public void setupBuildingDeck(int numPlayers) {
+        // implement
+    }
+
+    public void randomlyPlaceTotem() {
+        // implement
+    }
+
+    public void removeFromBoard(Card card, ArrayList<Card> upperRow, ArrayList<Card> bottomRow) {
+        // implement
+    }
+
+    public void repositionTotem() {
+        // "metodo per riportare totem su turnTile"
+    }
+
+    public void moveUpToDown() {
+        // "metodo sposto carte sopra a sotto tranne building"
+
+    }
+
+    public void discardBuildingsLowerEra3() {
+        // implement
+    }
+
+    public void moveBuildingsUpToDown() {
+        // implement
+    }
+
+    public void restoreNewEraBuildings() {
+        // implement
+    }
+
 }
--- src/main/java/it/polimi/ingsw/am46/model/Deck.java
+++ src/main/java/it/polimi/ingsw/am46/model/Deck.java
@@ -5,4 +5,31 @@ import java.util.Deque;
 
 public class Deck<T extends Card> {
     private Deque<T> Cards;
+
+
+    public Deck() {
+        // costruttore
+    }
+
+    public T draw() {
+        // pescare
+        return null;
+    }
+
+    public boolean isEmpty() {
+        // implement
+        return false;
+    }
+
+    public void shuffle() {
+        // rimescolamento (usando Collections.shuffle)
+    }
+
+    public void addCardToTop(T card) {
+        // inserire in testa
+    }
+
+    public void addCardToBottom(T card) {
+        // inserire in coda
+    }
 }
