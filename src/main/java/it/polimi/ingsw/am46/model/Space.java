72dd45c9b269d4f4fbe78c25fd67bed4e8f20321
Luca Cassani <luca1.cassani@mail.polimi.it>
2026-03-16 23:04:18 +0100

    aggiunta metodi in space.java

--- src/main/java/it/polimi/ingsw/am46/model/Space.java
+++ src/main/java/it/polimi/ingsw/am46/model/Space.java
@@ -1,4 +1,35 @@
 package it.polimi.ingsw.am46.model;
 
 public class Space {
+    private final int pos;
+    private final int food;
+    private final int pp;
+    private boolean occupied;
+
+    public Space(int pos, int food, int pp) {
+        this.pos = pos;
+        this.food = food;
+        this.pp = pp;
+        this.occupied = false;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getPos() {
+        return pos;
+    }
 }
