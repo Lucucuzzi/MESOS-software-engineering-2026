ed37a3b0d5f43d461e070cbd61c43bb319df7d18
Luca Cassani <luca1.cassani@mail.polimi.it>
2026-03-21 17:16:07 +0100

    Aggiunta classe DeckLoader.java

---
+++ src/main/java/it/polimi/ingsw/am46/model/DeckLoader.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.am46.model;
+
+import it.polimi.ingsw.am46.model.cards.buildingCards.BuildingCard;
+import it.polimi.ingsw.am46.model.cards.characterCards.CharacterCard;
+import it.polimi.ingsw.am46.model.cards.eventCards.EventCard;
+
+import java.util.List;
+
+public class DeckLoader {
+    public List<BuildingCard> loadBuildings(String jsonString){
+        return null;
+    }
+    public List<EventCard> loadEvents(String jsonString){
+        return null;
+    }
+
+    public List<CharacterCard> loadCharacters(String jsonString){
+        return null;
+    }
+
+}
