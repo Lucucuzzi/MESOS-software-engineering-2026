137ce6771be2b01a5d6cbdc377c4dc3868ab1b39
Luca Cassani <luca1.cassani@mail.polimi.it>
2026-03-21 16:07:25 +0100

    Aggiunta classe RoundPhase.java

---
+++ src/main/java/it/polimi/ingsw/am46/model/RoundPhase.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.am46.model;
+
+import it.polimi.ingsw.am46.model.cards.Card;
+
+public abstract class RoundPhase {
+    private TriggerType triggerType;
+
+    public TriggerType getTriggerType() {
+        return triggerType;
+    }
+
+    public void handlePlaceTotem(Player player, OfferTile offerTile){};
+
+    public void handleAddCard(Player player, Card card){};
+    public void handleResolveEvent(){};
+    public void handleEndRound(){};
+    public void nextPhase (){};
+    public void ApplyBuildingEffect(RoundPhase roundPhase, GameContext ctx){};
+}
