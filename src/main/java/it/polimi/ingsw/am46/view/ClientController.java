cf606735f35acce26be03d4f181479d3977b36c4
Luca Cassani <luca1.cassani@mail.polimi.it>
2026-04-19 15:52:51 +0200

    ClientController.java implementazione

--- src/main/java/it/polimi/ingsw/am46/view/ClientController.java
+++ src/main/java/it/polimi/ingsw/am46/view/ClientController.java
@@ -48,38 +48,91 @@ public class ClientController {
     public void onMoveTotem(String offerTileId) {
         // Check if it's the player's turn
         // If not, notify error locally and stop
+        if (!localModel.isMyTurn(myNickname)) {
+            localModel.notifyError("Non è il tuo turno!");
+            return;
+        }
 
         // Check if the tile is free
         // If not, notify error locally and stop
+        char letter = offerTileId.charAt(0);
+        if (!localModel.isTileFree(letter)) {
+            localModel.notifyError("Tile " + offerTileId + " is already occupied or does not exist.");
+            return;
+        }
 
         // Check if the current phase is PlaceTotemState
         // If not, notify error locally and stop
+        if (!localModel.isCurrentPhase("PlaceTotemState")) {
+            localModel.notifyError("You cannot place the totem in this phase of the game.");
+            return;
+        }
 
         // Send the moveTotem command to the server
         // If the network fails, notify error locally
+        try {
+            server.moveTotem(myNickname, offerTileId);
+        } catch (Exception e) {
+            localModel.notifyError("Network error while sending the move totem command.");
+        }
     }
 
     // Called when the user wants to take a card
     public void onAddCard(String cardId) {
         // Check if the current phase is AddCardState
         // If not, notify error locally and stop
+        if (!localModel.isMyTurn(myNickname)) {
+            localModel.notifyError("It's not your turn!!");
+            return;
+        }
 
-        // local validation for food cost
+        if (!localModel.isCurrentPhase("AddCardState")) {
+            localModel.notifyError("You cannot take a card in this phase of the game.");
+            return;
+        }
+
+        // validation for food cost in the server
 
         // Send the addCard command to the server
         // If the network fails, notify error locally
+        try {
+            server.addCard(myNickname, cardId);
+        } catch (Exception e) {
+            localModel.notifyError("Network error while trying to add the card.");
+        }
     }
+
 
     // Called when the user wants to take the extra card
     // If cardId is null, the user is skipping the extra draw
     public void onAddExtraCard(String cardId) {
         // Send the addExtraCard command to the server
         // If the network fails, notify error locally
+        if (!localModel.isMyTurn(myNickname)) {
+            localModel.notifyError("It's not your turn!!");
+            return;
+        }
+        try {
+            server.addExtraCard(myNickname, cardId);
+        } catch (Exception e) {
+            localModel.notifyError("Network error while trying to add the extra card.");
+        }
     }
+
 
     // Called when the user wants to skip the ExtraDraw phase
     public void onSkipExtraDraw() {
         // Send the skipExtraDraw command to the server
         // If the network fails, notify error locally
+        if (!localModel.isMyTurn(myNickname)) {
+            localModel.notifyError("It's not your turn!");
+            return;
+        }
+
+        try {
+            server.skipExtraDraw(myNickname);
+        } catch (Exception e) {
+            localModel.notifyError("Network error while trying to skip the extra draw.");
+        }
     }
 }
